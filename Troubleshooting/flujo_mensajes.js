9dbc0aa0573e5930538cd74503d6fe52b5c3062d
NicolasAyalaRosas <nicolasayalarosas@gmail.com>
2025-07-13 11:41:38 -0600

    Se crea extension Troubleshooting y se agrega solo al inicio para realizar pruebas

---
+++ Troubleshooting/flujo_mensajes.js
@@ -0,0 +1,14 @@
+export default {
+    texto: '¿Puede recibir mensajes?',
+    hijos: [
+        {
+            texto: 'Sí',
+            hijos: [
+                { texto: '¿El problema es con un número específico?' }
+            ]
+        },
+        {
+            texto: 'No → Revisar configuración del centro de mensajes'
+        }
+    ]
+};
