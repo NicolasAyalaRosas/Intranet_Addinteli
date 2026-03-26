e7dd5a90e62774c849c830f98a389221d99830a4
NicolasAyalaRosas <nicolasayalarosas@gmail.com>
2026-03-26 16:25:07 -0600

    Carga de Plantillas

--- Registros/Plantillas/Correo/Procesos NWM.js
+++ Registros/Plantillas/Correo/Procesos NWM.js
@@ -1,6 +1,17 @@
 window.PLANTILLAS.correo ["Procesos NWM"] = {
 
-"Cambio de Oferta - WM": `Estimado cliente, 
+    "Compra de adicionales": `Estimado Cliente,
+
+Le informamos que se realizó la compra del adicional solicitado de manera correcta.
+
+Solicitamos de su apoyo para validar con el usuario.
+
+Sin otro particular, quedamos al pendiente de sus comentarios.
+
+Saludos.`,
+
+
+    "Cambio de Oferta - WM": `Estimado cliente, 
 
 Conforme a lo solicitado, se aplicó cambio de oferta al número xxxxxx de manera correcta, solicitamos de su valioso apoyo para revisar con el usuario.
 
@@ -24,4 +35,5 @@ Conforme a lo solicitado, se realizó la migración de los números xxxxxx y xxx
 
 Quedamos al pendiente de sus comentarios.
 Saludos.`,
+
 };
